0e6b7dd91f927ec8e7783348e69fad228946f2da
SamuelOsp <avilaospina1@gmail.com>
2025-08-29 21:48:40 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mobile app exam',
+  webDir: 'www'
+};
+
+export default config;
